5efbebf22e234a59ac4ca2c0bdf76ef2d17bfd91
Johan Hultin <johan.hultin99@gmail.com>
2025-02-24 10:31:16 +0100

    swagger

--- Labb3-API/Program.cs
+++ Labb3-API/Program.cs
@@ -22,11 +22,10 @@ namespace Labb3_API
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+  
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            
 
             if (!app.Environment.IsDevelopment())
             {
